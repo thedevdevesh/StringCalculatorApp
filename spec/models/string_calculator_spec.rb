020fb6f342f051a8bc895fb3a7f49ce79117dc5c
devesh140901 <deveshkumarsingh99@gmail.com>
2025-01-10 13:25:30 +0530

    added test cases for String Calculator add method

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -14,4 +14,24 @@ require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :model do
     let(:calculator) { described_class.new }
+
+    describe '#add' do
+        context 'when the input is an empty string' do
+            it 'returns 0' do
+                expect(calculator.add('')).to eq(0)
+            end
+        end
+
+        context 'when the input is a single number string' do
+            it 'returns the number itself' do
+                expect(calculator.add('1')).to eq(1)
+            end
+        end
+
+        context 'when the input contains two numbers' do
+            it 'returns their sum' do
+                expect(calculator.add('1,5')).to eq(6)
+            end
+        end
+    end
 end
