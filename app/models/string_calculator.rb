982449c6db0df5b63af68f53d096accaba5af1ae
devesh140901 <deveshkumarsingh99@gmail.com>
2025-01-10 13:33:36 +0530

    define 'add' method in StringCalculator class and run the test cases.

---
+++ app/models/string_calculator.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+################################################################################
+#                             STRING CALCULATOR                                #
+#                                                                              #
+# This class is written by Devesh Kumar Singh                                  #
+#                                                                              #
+# Provides string calculator functionality:                                    #
+# - Can add numbers provided as a comma-separated string                       #
+# - Returns 0 for empty strings                                                #
+#                                                                              #
+################################################################################
+
+class StringCalculator
+  # Adds numbers from a comma-separated string.
+  # @param [String] A comma-separated string of numbers.
+  # @return [Integer] The sum of the numbers in the string.
+  def add(numbers)
+    return 0 if numbers.empty?
+
+    numbers.split(",").map(&:to_i).sum
+  end
+end
