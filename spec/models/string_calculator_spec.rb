9b61fadf20ff87745f21d21f37bb7c4305233e59
devesh140901 <deveshkumarsingh99@gmail.com>
2025-01-10 13:19:33 +0530

    initialise rspec test for string calculator

---
+++ spec/models/string_calculator_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+################################################################################
+#                             STRING CALCULATOR TESTS                          #
+#                                                                              #
+# This tests are written by Devesh Kumar Singh                                 #
+# This file contains tests for the string calculator class.                    #
+# These tests are written to validate  String Calculator                       #
+#                                                                              #
+################################################################################
+
+
+require 'rails_helper'
+
+RSpec.describe StringCalculator, type: :model do
+    let(:calculator) { described_class.new }
+end
